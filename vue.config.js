5833d286bb65d92152636de05ebf6e5f569184bd
jiamei.lai <jiamei.lai@ucarinc.com>
2023-06-09 20:11:31 +0800

    使用postcss-px-to-viewport做屏幕适配

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,24 @@
 const {
   defineConfig
 } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: "./",
   devServer: {
     host: "localhost",
     // host:"192.168.39.13",
-    open: true,
+    open: false,
   },
   configureWebpack: config => {
     // 为生产环境修改配置...
     if (process.env.NODE_ENV === 'production') {
-        config.mode = 'production';
-        // 打包文件大小配置
-        config.performance = {
-          maxEntrypointSize: 10000000,
-          maxAssetSize: 30000000
-        }
+      config.mode = 'production';
+      // 打包文件大小配置
+      config.performance = {
+        maxEntrypointSize: 10000000,
+        maxAssetSize: 30000000
+      }
     }
-},
+  },
 })
